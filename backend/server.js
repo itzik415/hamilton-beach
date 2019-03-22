22f58391fe26a0fa3b6949896ab9d82c53afd666
Itzik Shaoulian <itzikshaoulian@gmail.com>
2019-03-22 19:16:12 -0400

    fixing bugs

--- backend/server.js
+++ backend/server.js
@@ -69,13 +69,10 @@ if (process.env.NODE_ENV === 'production') {
     // Serve any static files
     app.use(express.static(path.join(__dirname, '/../frontend/build')));
   // Handle React routing, return all requests to React app
-    app.get('/', function(req, res) {
+    app.get('/*', function(req, res) {
       res.sendFile(path.join(__dirname, '/../frontend/build', 'index.html'));
     });
 }
-// app.get('/', function (req, res) {
-//     res.sendFile(path.join(__dirname, '/../frontend/public', 'index.html'));
-// });
 
 
 app.post('/api/pay', (req,res) => {
