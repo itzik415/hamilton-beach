b4d564e5baa26885204bf26628497fcb38cff85f
Itzik Shaoulian <itzikshaoulian@gmail.com>
2019-03-23 20:56:49 -0400

    fix

---
+++ frontend/build/precache-manifest.79f8e1702d3eb9b61af31ec1ce596972.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "119c644a1da8d98638ccf67cbd9a9ba6",
+    "url": "/static/media/sticker-model.119c644a.png"
+  },
+  {
+    "revision": "c3a66a06cdd01e059f6337695a847038",
+    "url": "/static/media/logo.c3a66a06.png"
+  },
+  {
+    "revision": "212c9e726c2dde991052b0ac3db55705",
+    "url": "/static/media/logo-black.212c9e72.png"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.c5541365.js"
+  },
+  {
+    "revision": "a1c525f46de6e4315412",
+    "url": "/static/js/main.ec65cdb8.chunk.js"
+  },
+  {
+    "revision": "924887cb724d3fabb158",
+    "url": "/static/js/2.0ede688c.chunk.js"
+  },
+  {
+    "revision": "a1c525f46de6e4315412",
+    "url": "/static/css/main.f9dbe2b8.chunk.css"
+  },
+  {
+    "revision": "ad2ae74ce5fdde1f7dc9f19dd44a654c",
+    "url": "/index.html"
+  }
+];
