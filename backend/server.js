3653faa133d47cafd2c8e2c64eb16a008bc959eb
Itzik Shaoulian <itzikshaoulian@gmail.com>
2019-03-23 22:36:22 -0400

    fix

--- backend/server.js
+++ backend/server.js
@@ -64,10 +64,13 @@ paypal.configure({
 
 if (process.env.NODE_ENV === 'production') {
     // Serve any static files
-    app.use(express.static(path.join(__dirname, '/../frontend/build')));
+        // app.get('*', (req, res) => {
+        //     res.sendFile(path.join(__dirname, '../frontend/build/index.html'));
+        // });
+    app.use(express.static(path.join(__dirname, '../frontend/build')));
   // Handle React routing, return all requests to React app
     app.get('/*', function(req, res) {
-      res.sendFile(path.join(__dirname, '/../frontend/build', 'index.html'));
+        res.sendFile(path.join(__dirname, '../frontend/build/index.html'));
     });
 }
 
