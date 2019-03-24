9d73b01cd71ed3e2338c2760acdbb3ec6e4c121b
Itzik Shaoulian <itzikshaoulian@gmail.com>
2019-03-23 21:03:06 -0400

    fix

--- backend/server.js
+++ backend/server.js
@@ -49,16 +49,16 @@ paypal.configure({
 // })
 
 if (process.env.NODE_ENV === "production") {
-        app.use(express.static('/../build'));
+        app.use(express.static('/../frontend/build'));
         app.get('/*', function(req, res) {
-            res.sendFile(path.join(__dirname, "/../build/index.html"));
+            res.sendFile(path.join(__dirname, "/../frontend/build/index.html"));
         });
     }
   
     else {
-        app.use(express.static(path.join(__dirname, '/../build')));
+        app.use(express.static(path.join(__dirname, '/../frontend/build')));
         app.get('/*', function(req, res) {
-            res.sendFile(path.join(__dirname, "/../build/index.html"));
+            res.sendFile(path.join(__dirname, "/../frontend/build/index.html"));
         });
 }
 
