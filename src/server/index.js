96e694c78ec4caa716d146feb75cd4e11a96b2c3
Itzik Shaoulian <itzikshaoulian@gmail.com>
2019-04-02 22:48:41 -0400

    changing package.json and .babelrc

--- src/server/index.js
+++ src/server/index.js
@@ -46,59 +46,6 @@ paypal.configure({
     'client_secret': process.env.PAYPAL_CLIENT_SECRET
 })
 
-if (process.env.NODE_ENV === 'production') {
-    app.get('/*', (req, res, next) => {
-        const activeRoute = routes.find((route) => matchPath(req.url, route)) || {}
-        // const composeEnhancers = global.window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
-        const store = createStore(rootReducer, applyMiddleware(thunk));
-
-        const preloadedState = store.getState();
-
-        const promise = activeRoute.fetchInitialData ? 
-            activeRoute.fetchInitialData(req.path): 
-            Promise.resolve()
-
-        promise.then((data) => {
-            const context = { data }
-            const markup = renderToString(
-                <Provider store={store}>
-                    <StaticRouter location={req.url} context={context}>
-                        <Main />    
-                    </StaticRouter>
-                </Provider>
-            )
-        
-            res.send(`
-                <!DOCTYPE html>
-                <html dir="rtl">
-                    <head>
-                        <meta charset="UTF-8">
-                        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-                        <meta http-equiv="X-UA-Compatible" content="ie=edge">
-                        <script src='/bundle.js' defer></script>
-                        <script>window.__INITIAL_DATA__ = ${serialize(data)}</script>
-                        
-                        <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.6.3/css/all.css" integrity="sha384-UHRtZLI+pbxtHCWp1t77Bi1L4ZtiqrqD80Kn4Z8NTSRyMA2Fd33n5dQ8lWUE00s/" crossorigin="anonymous">
-                        <link href="https://fonts.googleapis.com/css?family=Rubik:300,400,500,700,900" rel="stylesheet">
-                        <meta http-equiv="X-UA-Compatible" content="ie=edge">
-
-                        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
-                        <title>Hamilton Beach</title>
-                    </head>
-                    <body>
-                        <div id="root">${markup}</div>
-                        <script>window._PRELOADED_STATE__ = ${JSON.stringify(preloadedState).replace(/</g, '\\u003c')}</script>
-                        <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
-                        <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.6/umd/popper.min.js" integrity="sha384-wHAiFfRlMFy6i5SRaxvfOCifBUQy1xHdJ/yoi7FRNXMRBu5WHdZYu1hA6ZOblgut" crossorigin="anonymous"></script>
-                        <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/js/bootstrap.min.js" integrity="sha384-B0UglyR+jN6CkvvICOB2joaf5I4l3gm9GU6Hc1og6Ls7i6U/mkkaduKaBhlAXv9k" crossorigin="anonymous"></script>
-                        <script src="https://unpkg.com/ionicons@4.5.5/dist/ionicons.js"></script>
-                    </body>
-                </html>
-            `)
-        }).catch(next)
-    })
-}
-
 
 
 app.post('/api/pay', (req,res) => {
@@ -492,6 +439,59 @@ app.get('/api/cart/:email', (req,res) => {
     db.select('*').from('carts').where({email:req.params.email})
         .then(products => res.json(products))
 })
+
+// if (process.env.NODE_ENV === 'production') {
+    app.get('/*', (req, res, next) => {
+        const activeRoute = routes.find((route) => matchPath(req.url, route)) || {}
+        // const composeEnhancers = global.window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+        const store = createStore(rootReducer, applyMiddleware(thunk));
+
+        const preloadedState = store.getState();
+
+        const promise = activeRoute.fetchInitialData ? 
+            activeRoute.fetchInitialData(req.path): 
+            Promise.resolve()
+
+        promise.then((data) => {
+            const context = { data }
+            const markup = renderToString(
+                <Provider store={store}>
+                    <StaticRouter location={req.url} context={context}>
+                        <Main />    
+                    </StaticRouter>
+                </Provider>
+            )
+        
+            res.send(`
+                <!DOCTYPE html>
+                <html dir="rtl">
+                    <head>
+                        <meta charset="UTF-8">
+                        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+                        <meta http-equiv="X-UA-Compatible" content="ie=edge">
+                        <script src='/bundle.js' defer></script>
+                        <script>window.__INITIAL_DATA__ = ${serialize(data)}</script>
+                        
+                        <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.6.3/css/all.css" integrity="sha384-UHRtZLI+pbxtHCWp1t77Bi1L4ZtiqrqD80Kn4Z8NTSRyMA2Fd33n5dQ8lWUE00s/" crossorigin="anonymous">
+                        <link href="https://fonts.googleapis.com/css?family=Rubik:300,400,500,700,900" rel="stylesheet">
+                        <meta http-equiv="X-UA-Compatible" content="ie=edge">
+
+                        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+                        <title>Hamilton Beach</title>
+                    </head>
+                    <body>
+                        <div id="root">${markup}</div>
+                        <script>window._PRELOADED_STATE__ = ${JSON.stringify(preloadedState).replace(/</g, '\\u003c')}</script>
+                        <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+                        <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.6/umd/popper.min.js" integrity="sha384-wHAiFfRlMFy6i5SRaxvfOCifBUQy1xHdJ/yoi7FRNXMRBu5WHdZYu1hA6ZOblgut" crossorigin="anonymous"></script>
+                        <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/js/bootstrap.min.js" integrity="sha384-B0UglyR+jN6CkvvICOB2joaf5I4l3gm9GU6Hc1og6Ls7i6U/mkkaduKaBhlAXv9k" crossorigin="anonymous"></script>
+                        <script src="https://unpkg.com/ionicons@4.5.5/dist/ionicons.js"></script>
+                    </body>
+                </html>
+            `)
+        }).catch(next)
+    })
+// }
 
 const port = process.env.PORT || 5000;
 app.listen(port, () => {
